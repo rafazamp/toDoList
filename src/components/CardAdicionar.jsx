82a505315697b7910f04e5483157c85b5a65069a
rafazamp <rafaelprimieri@hotmail.com>
2023-03-21 16:16:29 -0300

    M1S08-Ex2

---
+++ src/components/CardAdicionar.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export const CardAdicionar = () => {
+  const [tarefa, setTarefa] = useState("")
+
+  const adicionarTarefa = (event) => {
+    event.preventDefault();
+    if (tarefa == "") {
+      alert("Digite uma tarefa válida");
+    } else {
+      alert(tarefa);
+      //Adicionar tarefa no array
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={tarefa}
+        onChange={(event) => setTarefa(event.target.value)}
+        placeholder="Digite o nome da tarefa..."
+      />
+
+      <button onClick={adicionarTarefa}>Adicionar Tarefa</button>
+    </div>
+  );
+}
