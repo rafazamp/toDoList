48c88c26ac13b37d1298f2289b84c2d8d32af58a
rafazamp <rafaelprimieri@hotmail.com>
2023-03-28 16:03:08 -0300

    M1S08-EX8

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export const ListItem = ({ tarefa }) => {
+export const ListItem = ({ finalizarTarefa, removerTarefa }) => {
   //Guarda o estado da tarefa (finalziada ou nao)
   const [tarefaFinalizada, setTarefaFinalizada] = useState(true);
 
@@ -9,9 +9,15 @@ export const ListItem = ({ tarefa }) => {
     setTarefaFinalizada(!tarefaFinalizada);
   }
 
+  function removerTarefa(event) {
+    event.preventDefault;
+    removerTarefa(tarefa);
+  }
+
   return (
     <div>
       <button onClick={finalizarTarefa}>Finalizar Tarefa</button>
+      <button onClick={removerTarefa}>Remover Tarefa</button>
     </div>
   );
 };
